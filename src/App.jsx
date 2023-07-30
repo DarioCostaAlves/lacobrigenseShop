345cb74b71d492af4dd38c1bb857edbcf8d4f2d8
DarioCostaAlves <dario.c.alves22@gmail.com>
2023-07-30 16:55:32 +0100

    lacobrigense shop tailwind t itle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <h1 class="text-9xl font-bold">Oláa</h1>
+        <h1 class="text-5xl font-bold">Lacobrigense Shop</h1>
       </div>
     </>
   );
